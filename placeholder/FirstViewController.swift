99cbc3b7123392f57df578cd2e826b6971f40fad
russellry <digivicer@hotmail.com>
2020-02-22 11:07:41 +0800

    Initial Commit

---
+++ placeholder/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  placeholder
+//
+//  Created by Russell Ong on 22/2/20.
+//  Copyright © 2020 h4h. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
