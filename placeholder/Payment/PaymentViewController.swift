29e4ec395484ec546899cc969dc43e9afbf04765
russellry <digivicer@hotmail.com>
2020-02-23 13:42:04 +0800

    Integrated Stripe API

--- placeholder/Payment/PaymentViewController.swift
+++ placeholder/Payment/PaymentViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import Stripe
+let BackendUrl = "http://127.0.0.1:4242/"
 
 class PaymentViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var amountTextField: UITextField!
+    var paymentIntentClientSecret: String?
+    lazy var cardTextField: STPPaymentCardTextField = {
+        let cardTextField = STPPaymentCardTextField()
+        return cardTextField
+    }()
+    
+    @IBOutlet weak var payButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +28,88 @@ class PaymentViewController: UIViewController, UITextFieldDelegate {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange), name: UIResponder.keyboardWillHideNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        startCheckout()
+        payButton.addTarget(self, action: #selector(pay), for: .touchUpInside)
+
+    }
+    
+    func displayAlert(title: String, message: String, restartDemo: Bool = false) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            if restartDemo {
+                alert.addAction(UIAlertAction(title: "Restart demo", style: .cancel) { _ in
+                    self.startCheckout()
+                })
+            }
+            else {
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+            }
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func startCheckout() {
+        // Create a PaymentIntent by calling the sample server's /create-payment-intent endpoint.
+        let url = URL(string: BackendUrl + "create-payment-intent")!
+        let json: [String: Any] = [
+            "currency": "usd",
+            "items": [
+                ["id": "photo_subscription", "price": "5.00"]
+            ]
+        ]
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONSerialization.data(withJSONObject: json)
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { [weak self] (data, response, error) in
+            guard let response = response as? HTTPURLResponse,
+                response.statusCode == 200,
+                let data = data,
+                let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String : Any],
+                let clientSecret = json["clientSecret"] as? String,
+                let publishableKey = json["publishableKey"] as? String else {
+                    let message = error?.localizedDescription ?? "Failed to decode response from server."
+                    self?.displayAlert(title: "Error loading page", message: message)
+                    return
+            }
+            print("Created PaymentIntent")
+            self?.paymentIntentClientSecret = clientSecret
+            // Configure the SDK with your Stripe publishable key so that it can make requests to the Stripe API
+            // For added security, our sample app gets the publishable key from the server
+            Stripe.setDefaultPublishableKey(publishableKey)
+        })
+        task.resume()
+    }
+    
+    @objc
+    func pay() {
+        guard let paymentIntentClientSecret = paymentIntentClientSecret else {
+            return;
+        }
+        // Collect card details
+        let cardParams = cardTextField.cardParams
+        let paymentMethodParams = STPPaymentMethodParams(card: cardParams, billingDetails: nil, metadata: nil)
+        let paymentIntentParams = STPPaymentIntentParams(clientSecret: paymentIntentClientSecret)
+        paymentIntentParams.paymentMethodParams = paymentMethodParams
+
+        // Submit the payment
+        let paymentHandler = STPPaymentHandler.shared()
+        paymentHandler.confirmPayment(withParams: paymentIntentParams, authenticationContext: self) { (status, paymentIntent, error) in
+            switch (status) {
+            case .failed:
+                self.displayAlert(title: "Payment failed", message: error?.localizedDescription ?? "")
+                break
+            case .canceled:
+                self.displayAlert(title: "Payment canceled", message: error?.localizedDescription ?? "")
+                break
+            case .succeeded:
+                self.displayAlert(title: "Payment succeeded", message: paymentIntent?.description ?? "", restartDemo: true)
+                break
+            @unknown default:
+                fatalError()
+                break
+            }
+        }
     }
     
     deinit {
@@ -41,5 +132,11 @@ class PaymentViewController: UIViewController, UITextFieldDelegate {
     }
     @IBAction func payClicked(_ sender: Any) {
         hideKeyboard()
+    }
+}
+
+extension PaymentViewController: STPAuthenticationContext {
+    func authenticationPresentingViewController() -> UIViewController {
+        return self
     }
 }
